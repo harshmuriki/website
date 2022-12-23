a1af7f03ef9f2169428554e4ad6a214f4f8e69fb
harshmuriki24 <vmuriki3@gatech.edu>
2022-12-23 15:44:17 -0800

    Changed name to Harsh

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'React Resume Template - Harsh Muriki',
+  description: "Example site built with Venkata Harsh Suhith Muriki's react resume template",
 };
 
 /**
@@ -69,7 +69,7 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Harsh Muriki.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
