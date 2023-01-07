683be882ff7ea0c8c1116decc87f879a1d097061
harshmuriki <harshsuhith@gmail.com>
2023-01-06 16:44:30 -0800

    File changes to make it more personalized

--- src/components/Sections/Portfolio.tsx
+++ src/components/Sections/Portfolio.tsx
@@ -12,8 +12,8 @@ import Section from '../Layout/Section';
 const Portfolio: FC = memo(() => {
   return (
     <Section className="bg-neutral-800" sectionId={SectionId.Portfolio}>
-      <div className="flex flex-col gap-y-8">
-        <h2 className="self-center text-xl font-bold text-white">Check out some of my work</h2>
+      <div className="flex flex-col gap-y-9 -my-12">
+        <div className="self-center text-3xl font-bold text-white">Check out some of my work!</div>
         <div className=" w-full columns-2 md:columns-3 lg:columns-4">
           {portfolioItems.map((item, index) => {
             const {title, image} = item;
---
+++ src/components/Sections/imagescroll.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+interface ImageScrollProps {
+  images: string[];
+}
+
+const ImageScroll: React.FunctionComponent<ImageScrollProps> = ({ images }) => {
+  const [currentImage, setCurrentImage] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((currentImage + 1) % images.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [currentImage, images.length]);
+
+  return (
+    <div className="image-scroll">
+      <img src={images[currentImage]} alt="" />
+    </div>
+  );
+};
+
+export default ImageScroll;
